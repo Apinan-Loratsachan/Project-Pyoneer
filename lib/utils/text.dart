d36a3f5947ee0f0fb98d2b98f0f580ba579b6083
Apinan Loratsachan <sunseasame2556@gmail.com>
2024-01-11 13:22:08 +0700

    Text size function.

--- lib/utils/text.dart
+++ lib/utils/text.dart
@@ -1,8 +1,18 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 class PyoneerText {
   static const double titleTextSize = 40;
   static const double bodyTextSize = 20;
   static const double textSpaceSize = 25;
   static const String startParagraph = "\n\t\t\t\t\t\t\t\t\t\t\t";
+
+  static Text bodyText(String text, [double fontSize = bodyTextSize, FontWeight fontWeight = FontWeight.normal]) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight,
+      ),
+    );
+  }
 }
