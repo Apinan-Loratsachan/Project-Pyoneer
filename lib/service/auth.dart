2d3ae15494063986a3c3307c75e96618a00e95d2
Kanttz <krisorn225@gmail.com>
2024-01-12 07:02:40 +0700

    Refactor sign-in with Google in auth.dart

--- lib/service/auth.dart
+++ lib/service/auth.dart
@@ -1,19 +1,28 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 Future<UserCredential?> signInWithGoogle() async {
-  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  try {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-  if (googleUser == null) {
+    if (googleUser == null) {
+      return null;
+    }
+
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  } catch (e) {
+    if (kDebugMode) {
+      print('Error signing in with Google: $e');
+    }
     return null;
   }
-
-  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-
-  final credential = GoogleAuthProvider.credential(
-    accessToken: googleAuth.accessToken,
-    idToken: googleAuth.idToken,
-  );
-
-  return FirebaseAuth.instance.signInWithCredential(credential);
 }
